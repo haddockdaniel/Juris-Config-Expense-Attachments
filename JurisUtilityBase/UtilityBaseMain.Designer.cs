049acce531ac80b4db2c916b5dfa81357acc81ce
haddockdaniel <43544084+haddockdaniel@users.noreply.github.com>
2019-03-09 12:45:10 -0500

    Initial commit

---
+++ JurisUtilityBase/UtilityBaseMain.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace JurisUtilityBase
+{
+    partial class UtilityBaseMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UtilityBaseMain));
+            this.JurisLogoImageBox = new System.Windows.Forms.PictureBox();
+            this.LexisNexisLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBoxCompanies = new System.Windows.Forms.ListBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.statusGroupBox = new System.Windows.Forms.GroupBox();
+            this.labelCurrentStatus = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelPercentComplete = new System.Windows.Forms.Label();
+            this.OpenFileDialogOpen = new System.Windows.Forms.OpenFileDialog();
+            this.buttonReport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButtonCode0 = new System.Windows.Forms.RadioButton();
+            this.radioButtonCode1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonCode2 = new System.Windows.Forms.RadioButton();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.statusGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // JurisLogoImageBox
+            // 
+            this.JurisLogoImageBox.Image = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.Image")));
+            this.JurisLogoImageBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.InitialImage")));
+            this.JurisLogoImageBox.Location = new System.Drawing.Point(0, 1);
+            this.JurisLogoImageBox.Name = "JurisLogoImageBox";
+            this.JurisLogoImageBox.Size = new System.Drawing.Size(104, 336);
+            this.JurisLogoImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.JurisLogoImageBox.TabIndex = 0;
+            this.JurisLogoImageBox.TabStop = false;
+            // 
+            // LexisNexisLogoPictureBox
+            // 
+            this.LexisNexisLogoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("LexisNexisLogoPictureBox.Image")));
+            this.LexisNexisLogoPictureBox.Location = new System.Drawing.Point(8, 343);
+            this.LexisNexisLogoPictureBox.Name = "LexisNexisLogoPictureBox";
+            this.LexisNexisLogoPictureBox.Size = new System.Drawing.Size(96, 28);
+            this.LexisNexisLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.LexisNexisLogoPictureBox.TabIndex = 1;
+            this.LexisNexisLogoPictureBox.TabStop = false;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 374);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(658, 22);
+            this.statusStrip.TabIndex = 2;
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(134, 17);
+            this.toolStripStatusLabel.Text = "Status: Ready to Execute";
+            // 
+            // listBoxCompanies
+            // 
+            this.listBoxCompanies.FormattingEnabled = true;
+            this.listBoxCompanies.Location = new System.Drawing.Point(111, 1);
+            this.listBoxCompanies.Name = "listBoxCompanies";
+            this.listBoxCompanies.Size = new System.Drawing.Size(262, 69);
+            this.listBoxCompanies.TabIndex = 0;
+            this.listBoxCompanies.SelectedIndexChanged += new System.EventHandler(this.listBoxCompanies_SelectedIndexChanged);
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelDescription.Location = new System.Drawing.Point(380, 1);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(268, 69);
+            this.labelDescription.TabIndex = 1;
+            this.labelDescription.Text = "Use this tool to globally configure expense attachment settings. his changes ALL " +
+    "clients and matters";
+            // 
+            // statusGroupBox
+            // 
+            this.statusGroupBox.Controls.Add(this.labelCurrentStatus);
+            this.statusGroupBox.Controls.Add(this.progressBar);
+            this.statusGroupBox.Controls.Add(this.labelPercentComplete);
+            this.statusGroupBox.Location = new System.Drawing.Point(111, 77);
+            this.statusGroupBox.Name = "statusGroupBox";
+            this.statusGroupBox.Size = new System.Drawing.Size(537, 73);
+            this.statusGroupBox.TabIndex = 5;
+            this.statusGroupBox.TabStop = false;
+            this.statusGroupBox.Text = "Utility Status:";
+            // 
+            // labelCurrentStatus
+            // 
+            this.labelCurrentStatus.AutoSize = true;
+            this.labelCurrentStatus.Location = new System.Drawing.Point(7, 50);
+            this.labelCurrentStatus.Name = "labelCurrentStatus";
+            this.labelCurrentStatus.Size = new System.Drawing.Size(77, 13);
+            this.labelCurrentStatus.TabIndex = 2;
+            this.labelCurrentStatus.Text = "Current Status:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(7, 27);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(519, 20);
+            this.progressBar.TabIndex = 0;
+            // 
+            // labelPercentComplete
+            // 
+            this.labelPercentComplete.AutoSize = true;
+            this.labelPercentComplete.Location = new System.Drawing.Point(413, 11);
+            this.labelPercentComplete.Name = "labelPercentComplete";
+            this.labelPercentComplete.Size = new System.Drawing.Size(62, 13);
+            this.labelPercentComplete.TabIndex = 0;
+            this.labelPercentComplete.Text = "% Complete";
+            // 
+            // OpenFileDialogOpen
+            // 
+            this.OpenFileDialogOpen.FileName = "openFileDialog1";
+            // 
+            // buttonReport
+            // 
+            this.buttonReport.BackColor = System.Drawing.Color.LightGray;
+            this.buttonReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReport.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonReport.Location = new System.Drawing.Point(133, 321);
+            this.buttonReport.Name = "buttonReport";
+            this.buttonReport.Size = new System.Drawing.Size(148, 38);
+            this.buttonReport.TabIndex = 16;
+            this.buttonReport.Text = "Exit";
+            this.buttonReport.UseVisualStyleBackColor = false;
+            this.buttonReport.Click += new System.EventHandler(this.buttonReport_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Location = new System.Drawing.Point(489, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 38);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButtonCode0
+            // 
+            this.radioButtonCode0.AutoSize = true;
+            this.radioButtonCode0.Location = new System.Drawing.Point(133, 176);
+            this.radioButtonCode0.Name = "radioButtonCode0";
+            this.radioButtonCode0.Size = new System.Drawing.Size(241, 17);
+            this.radioButtonCode0.TabIndex = 18;
+            this.radioButtonCode0.TabStop = true;
+            this.radioButtonCode0.Text = "Print attached expense files when printing bills";
+            this.radioButtonCode0.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCode1
+            // 
+            this.radioButtonCode1.AutoSize = true;
+            this.radioButtonCode1.Location = new System.Drawing.Point(133, 221);
+            this.radioButtonCode1.Name = "radioButtonCode1";
+            this.radioButtonCode1.Size = new System.Drawing.Size(275, 17);
+            this.radioButtonCode1.TabIndex = 19;
+            this.radioButtonCode1.TabStop = true;
+            this.radioButtonCode1.Text = "Do not print attached expense files when printing bills";
+            this.radioButtonCode1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCode2
+            // 
+            this.radioButtonCode2.AutoSize = true;
+            this.radioButtonCode2.Location = new System.Drawing.Point(133, 271);
+            this.radioButtonCode2.Name = "radioButtonCode2";
+            this.radioButtonCode2.Size = new System.Drawing.Size(337, 17);
+            this.radioButtonCode2.TabIndex = 20;
+            this.radioButtonCode2.TabStop = true;
+            this.radioButtonCode2.Text = "Print attached expense files when printing bills for expenses over $";
+            this.radioButtonCode2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(466, 271);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAmount.TabIndex = 21;
+            this.textBoxAmount.Text = "0.00";
+            // 
+            // UtilityBaseMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 396);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.radioButtonCode2);
+            this.Controls.Add(this.radioButtonCode1);
+            this.Controls.Add(this.radioButtonCode0);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonReport);
+            this.Controls.Add(this.statusGroupBox);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.listBoxCompanies);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.LexisNexisLogoPictureBox);
+            this.Controls.Add(this.JurisLogoImageBox);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UtilityBaseMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JPS - Config Expense Attachments";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.statusGroupBox.ResumeLayout(false);
+            this.statusGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox JurisLogoImageBox;
+        private System.Windows.Forms.PictureBox LexisNexisLogoPictureBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ListBox listBoxCompanies;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.GroupBox statusGroupBox;
+        private System.Windows.Forms.Label labelCurrentStatus;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelPercentComplete;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialogOpen;
+        private System.Windows.Forms.Button buttonReport;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButtonCode0;
+        private System.Windows.Forms.RadioButton radioButtonCode1;
+        private System.Windows.Forms.RadioButton radioButtonCode2;
+        private System.Windows.Forms.TextBox textBoxAmount;
+    }
+}
+
